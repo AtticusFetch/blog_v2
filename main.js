30d47f7795f9834aefd258428c8c87e4342fd80e
AtticusFetch <lordofthecucumbers@gmail.com>
2015-03-18 22:58:06 +0200

    fix content displaying

--- main.js
+++ main.js
@@ -1,30 +1,47 @@
-var currentTitle = 'main';
+var currentTitle = '';
 
 $(document).ready(function(){
     init();
-    clickRotate();
-})
+    clickMenuItem();
+});
 
 function init(){
     $('body').css('opacity','1');
     setTitle('main');
     $('.header').css('height',$('.wrapper .header #' + currentTitle).outerHeight(true) + 'px');
+    $('.wrapper .container .aboutContent').addClass('aboutVisible')
+            /*.css({left: $('.wrapper .menu ul').outerWidth(true)})
+                .css({right: 'auto'})*/;
+    alert($('.wrapper .menu ul li').outerWidth(true));
+    $('.wrapper .container').css({left: ($('.header').outerWidth(true) - $('.wrapper .container').outerWidth(true))/2 + $('.wrapper .menu ul li').outerWidth(true)})
+        .css({right: ($('.header').outerWidth(true) - $(this).outerWidth(true))/2});
+
 }
 
 function setTitle(titleID){
     currentTitle = titleID;
     $('.wrapper .header #' + titleID).addClass('visibleTitle')
-        .css({left: ($('header').outerWidth(true) - $(this).outerWidth(true))/2})
-            .css({right: ($('header').outerWidth(true) - $(this).outerWidth(true))/2})
+        .css({left: ($('.header').outerWidth(true) - $(this).outerWidth(true))/2})
+            .css({right: ($('.header').outerWidth(true) - $(this).outerWidth(true))/2});
 }
 
-function clickRotate(){
+function clickMenuItem(){
+    $('.wrapper .header').click(function(){
+        removeTitle(currentTitle);
+        init();
+    });
     $('.wrapper .menu ul li').click(function(){
-        $(this).addClass('rotate');
-        $(this).mouseout(function(){
-            $(this).removeClass('rotate');
+        removeTitle(currentTitle);
+        setTitle($(this).attr('class'));
+        $(this).addClass('rotate')
+            .mouseout(function(){
+                $(this).removeClass('rotate');
         });
     });
+}
+
+function removeTitle(titleID){
+    $('.wrapper .header #' + titleID).removeClass('visibleTitle');
 }
 
 /**
