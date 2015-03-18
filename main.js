f3689d6496e65605c5eeeb7e3a1f6c1ec6f6b18e
AtticusFetch <lordofthecucumbers@gmail.com>
2015-03-18 13:01:27 +0200

    epam2 3.18.15

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+var currentTitle = 'main';
+
+$(document).ready(function(){
+    init();
+})
+
+function init(){
+    $('body').css('opacity','1');
+    setTitle('main');
+    $('.header').css('height',$('.wrapper .header #' + currentTitle).outerHeight(true) + 'px');
+}
+
+function setTitle(titleID){
+    currentTitle = titleID;
+    $('.wrapper .header #' + titleID).addClass('visibleTitle')
+        .css({left: ($('header').outerWidth(true) - $('.wrapper .header #' + titleID).outerWidth(true))/2})
+            .css({right: ($('header').outerWidth(true) - $('.wrapper .header #' + titleID).outerWidth(true))/2})
+}
+
 /**
  * Created by Ivan_Iankovskyi on 3/18/2015.
  */
