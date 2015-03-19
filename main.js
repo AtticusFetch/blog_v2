81e3088bf723dc9636a31d8a89336ec8defc0aed
AtticusFetch <lordofthecucumbers@gmail.com>
2015-03-19 18:51:06 +0200

    font resize

--- main.js
+++ main.js
@@ -1,44 +1,60 @@
 var currentTitle = '';
+var currentWidth;
+var currentFontSize;
 
 $(document).ready(function(){
     init();
     clickMenuItem();
+    fontResize();
 });
 
 function init(){
     $('body').css('opacity','1');
     setTitle('main');
     $('.header').css('height',$('.wrapper .header #' + currentTitle).outerHeight(true) + 'px');
-            /*.css({left: $('.wrapper .menu ul').outerWidth(true)})
-                .css({right: 'auto'})*/
-    //alert($('.wrapper .menu ul li').outerWidth(true));
 }
 
 function setTitle(titleID){
+    currentWidth = $(window).width();
     currentTitle = titleID;
     $('.wrapper .header #' + titleID).addClass('visibleTitle')
         .css({left: ($('.header').outerWidth(true) - $(this).outerWidth(true))/2})
             .css({right: ($('.header').outerWidth(true) - $(this).outerWidth(true))/2});
+
+
+}
+
+function fontResize(){
+    currentWidth = $(window).width();
+    $(window).resize(function() {
+        $('#' + currentTitle + 'Content').css('font-size', parseInt(currentFontSize)*($(window).width()/parseInt(currentWidth)));
+    });
 }
 
 function clickMenuItem(){
+
     $('.wrapper .header').click(function(){
         removeTitle(currentTitle);
         $('#' + currentTitle + 'Content').removeClass(currentTitle + 'Visible');
         init();
     });
+
+
     $('.wrapper .menu ul li').click(function(){
-        //alert($(this).attr('class'));
+        //alert(currentTitle);
+        /*currentFontSize = $('#' + currentTitle + 'Content').css('font-size');
+        alert(currentFontSize);*/
+        $(this).removeClass('rotate');
         $('#' + currentTitle + 'Content').removeClass(currentTitle + 'Visible');
         $('#' + $(this).attr('class') + 'Content').addClass($(this).attr('class') + 'Visible');
-        $('#' + $(this).attr('class') + 'Content').css('left', ($(document).width() - $('#' + $(this).attr('class') + 'Content').outerWidth())/2 +'px')
-            .css('right',($(document).width() - $('#' + $(this).attr('class') + 'Content').outerWidth())/2 + 'px');
+        $('#' + $(this).attr('class') + 'Content').css('left', ($(window).width() - $('#' + $(this).attr('class') + 'Content').outerWidth())/2 +'px')
+            .css('right',($(window).width() - $('#' + $(this).attr('class') + 'Content').outerWidth())/2 + 'px');
         removeTitle(currentTitle);
         setTitle($(this).attr('class'));
         $(this).addClass('rotate')
             .mouseout(function(){
-                $(this).removeClass('rotate');
-        });
+                currentFontSize = $('#' + currentTitle + 'Content').css('font-size');
+            });
     });
 }
 
